1ac680d6e5310140a4b029fd62168b1ac2186747
aaronsomo <aaronsomo@users.noreply.github.com>
2021-05-17 22:59:36 +0000

    sets up coming-soon page dbcb2cd98bf03793af13129005220f1c11e76c90

---
+++ precache-manifest.985ddcc934b25b72a5a8cb940150d007.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ee627aa62ae2b6db7aed722d87deac",
+    "url": "/jametrades/index.html"
+  },
+  {
+    "revision": "b957389ec1ba58315793",
+    "url": "/jametrades/static/css/main.2c62ca77.chunk.css"
+  },
+  {
+    "revision": "419301298bd599d18186",
+    "url": "/jametrades/static/js/2.9e4cf306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jametrades/static/js/2.9e4cf306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b957389ec1ba58315793",
+    "url": "/jametrades/static/js/main.278ba5e2.chunk.js"
+  },
+  {
+    "revision": "6c7c68c0499460ac6f1b",
+    "url": "/jametrades/static/js/runtime-main.81893d0a.js"
+  },
+  {
+    "revision": "5159a5d89abe8bf68b09b569dbeccbc0",
+    "url": "/jametrades/static/media/IBMPlexSans-Bold.5159a5d8.ttf"
+  },
+  {
+    "revision": "ee425cc83f37323665790c89758cf359",
+    "url": "/jametrades/static/media/IBMPlexSans-BoldItalic.ee425cc8.ttf"
+  },
+  {
+    "revision": "40bbef08ca6f6edea2a9a9e882541ce0",
+    "url": "/jametrades/static/media/IBMPlexSans-Italic.40bbef08.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "/jametrades/static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "eb7dadea8e7c37ce1a1406045dda7c1e",
+    "url": "/jametrades/static/media/IBMPlexSans-MediumItalic.eb7dadea.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/jametrades/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "50f13391babe5b650b8a0589dd962f74",
+    "url": "/jametrades/static/media/facebook.50f13391.svg"
+  },
+  {
+    "revision": "baf48a32fd45986dfb5b52aada1b558a",
+    "url": "/jametrades/static/media/github.baf48a32.svg"
+  },
+  {
+    "revision": "a50716dd845ea43407a9a1bcffe42aef",
+    "url": "/jametrades/static/media/twitter.a50716dd.svg"
+  }
+]);
